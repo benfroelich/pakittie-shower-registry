9f2439e85975cfdae925f620801af723dabfba13
Ben FroelichLeon <benfroelich@gmail.com>
2018-07-04 17:48:37 +0000

    make seed database only add entries, and add 7 new entries as per Pakittie

--- utilities/seedDb.js
+++ utilities/seedDb.js
@@ -1,8 +1,7 @@
 require("mongoose");
 var RegistryEntry = require("../models/registryEntry");
 var seeds = [
-    // 36 + handmade
-    {item: "handmade goods", imgUrl: "/img/diy.jpg", link: "#", quantity: Number.POSITIVE_INFINITY}, 
+/*    {item: "handmade goods", imgUrl: "/img/diy.jpg", link: "#", quantity: Number.POSITIVE_INFINITY}, 
     {item: "wipes", imgUrl: "/img/wipes.jpg", link: "#", quantity: 4}, 
     {item: "Infant car seat", imgUrl: "/img/car-seat.jpg", link: "#"}, 
     {item: "Convertible car seat", imgUrl: "/img/convertible-car-seat.jpeg", link: "#"}, 
@@ -26,25 +25,38 @@ var seeds = [
     {item: "Pacifier", imgUrl: "/img/pacifier.jpeg", link: "#"},
     {item: "Nail clipper and nail filer", imgUrl: "/img/nail-clipper.jpeg", link: "#"},
     {item: "Automatic Bouncer/Auto rock and play sleeper", imgUrl: "/img/rocker.jpeg", link: "#"},
-    {item: "Wash cloths", imgUrl: "/img/towels.png", link: "#"}
+    {item: "Wash cloths", imgUrl: "/img/towels.png", link: "#"},
+*/    
+    {item: "Newborn clothes", imgUrl: "/img/newborn-clothes.jpg", link: "#", quantity: 2},
+    {item: "3-6 month clothes", imgUrl: "/img/three-to-six-month-clothes.jpg", link: "#", quantity: 2},
+    {item: ">6 month clothes", imgUrl: "/img/six-plus-month-clothes.jpg", link: "#", quantity: 2},
+    {item: "Crib sheets", imgUrl: "/img/crib-sheet.jpg", link: "#"},
+    {item: "Nursing cover", imgUrl: "/img/nursing-cover.jpg", link: "#"},
+    {item: "Diaper Genie", imgUrl: "/img/diaper-genie.jpg", link: "#"},
+    {item: "Baby healthcare kit", imgUrl: "/img/", link: "#"}
 ];
 function seedDb() {
     RegistryEntry.remove({}, function(err) {
         if(err) console.log(err);
         else {
-            console.log("remove registry entries");
+            console.log("remove and apply registry entries");
             seeds.forEach(function(seed) {
                 RegistryEntry.create(seed, function(err, newEntry) {
                     if(err) console.log(err);
-                    else 
-                    {
-                        console.log("created new entry:");
-                        console.log(newEntry);
-                    }
                });
             });
         }
     });
 }
 
-module.exports = seedDb;
+function addToDb() {
+    console.log("adding new items to database");
+    seeds.forEach(function(seed) {
+        RegistryEntry.create(seed, function(err, newEntry) {
+            if(err) console.log(err);
+       });
+    });
+}
+
+// change addToDb to seedDb to overwrite the items
+module.exports = addToDb;
